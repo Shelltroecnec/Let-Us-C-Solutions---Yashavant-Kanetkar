a09f203049f00aa4ff34a230b8979d0994f93d1b
Shahid <101791916+Shelltroecnec@users.noreply.github.com>
2025-05-17 15:57:40 +0530

    Update [H]-(d).c
    Updated the Question and added the Comments

--- 2 - C Instructions/[H]-(d).c
+++ 2 - C Instructions/[H]-(d).c
@@ -1,12 +1,17 @@
 /* [H] - (d) */
+/* Consider a currency system in which there are notes of seven denominations, namely, Rs. 1, Rs. 2, Rs.5, Rs. 10, Rs. 50, Rs. 100.
+   If a sum of Rs. N is enterd through the keyboard, write a program to compute the smallest number of notes that will combine to give Rs. N */
 
 #include<stdio.h>
 int main(){
+	// Declaring the variables
 	int i100, i50, i10, i5, i2, i1, iamount;
-	
-	printf("\n Enter the Amount to be Withdrawn =\n");
+
+	// Taking the Amount as input from the user
+	printf("\n Enter the Amount to be Withdrawn =");
 	scanf("%d",&iamount);
-	
+
+	// Calculating the Position
 	i100 = iamount/100;
 	iamount%=100;
 	
@@ -32,4 +37,5 @@ int main(){
 	printf("Total Rs5 Notes = %dRs\n",i5);
 	printf("Total Rs2 Notes = %dRs\n",i2);
 	printf("Total Rs1 Notes = %dRs\n",i1);
+	return 0;
 }
