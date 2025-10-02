0b93366ab0f235d0bb4af970b1fc2dd3de972340
Shahid Hashmi <shelltroecnec@gmail.com>
2025-10-02 16:04:18 +0000

    Updated the Question

--- 1 - Getting Started/[C]-(a).c
+++ 1 - Getting Started/[C]-(a).c
@@ -1,7 +1,14 @@
 /* [C] - (a) */
 
+/* Ramesh basic salary is input through the keyboard. His dearness allowance is 40% of basic salary,
+	and house rent allowance is 20% of basic salary. Write a program to calculate his gross salary */
+
+// Header	
 #include<stdio.h>
+
 int main(){
+
+	//Local Declarations
 	float fbasic_sal, f_allowance, f_house, fgross_sal=0;
 	
 	printf("\n\t***Gross Salary Calculator**");
@@ -11,4 +18,5 @@ int main(){
 	f_house = (fbasic_sal/100)*20;
 	fgross_sal = fbasic_sal + f_allowance + f_house;
 	printf("\nGross Salary :Rs %.2f",fgross_sal);
+	return 0;
 }
