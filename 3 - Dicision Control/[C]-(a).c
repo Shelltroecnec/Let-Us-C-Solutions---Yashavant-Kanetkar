b950a95bd0523e4fbfa5b2b521365dde53585896
Shahid Hashmi <shelltroecnec@gmail.com>
2025-10-04 12:14:23 +0000

    Updated Code

--- 3 - Dicision Control/[C]-(a).c
+++ 3 - Dicision Control/[C]-(a).c
@@ -1,12 +1,19 @@
 /* [C] - (a) */
 
+
+//Headers
 #include<stdio.h>
+
 int main(){
+
+	//Local declarations
 	int icp, isp, iprofit=0,iloss=0;
+
 	printf("Enter the Cost Price of the Product =");
 	scanf("%d",&icp);
 	printf("\nEnter the Selling Price of the Product =");
 	scanf("%d",&isp);
+	
 	if(isp > icp){
 		iprofit = isp - icp;
 		printf("\nProfit Earned = %dRs",iprofit);
@@ -16,4 +23,6 @@ int main(){
 		printf("\nYour Cost Price = %d\nSelling Price = %d",icp,isp);
 		printf("\nYou earned no Profit, Your Loss = %dRs",iloss);
 	}
+
+	return 0;
 }
