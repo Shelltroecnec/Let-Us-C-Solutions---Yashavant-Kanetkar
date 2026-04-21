a2c7267f52b25557c50cd067226815d4b91ee2bc
Shahid Hashmi <shelltroecnec@gmail.com>
2026-04-21 15:34:18 +0000

    Resumed the Problem solving and completed the question [G] - (a). And updated the Readme.md.

--- 6 - Functions & Pointers/[G] - (a).c
+++ 6 - Functions & Pointers/[G] - (a).c
@@ -1,9 +1,13 @@
 /* [G] - (a) */
 
-#include<stdio.h>
-#include<stdlib.h>
+/* Write a function that recieves 5 integers and returns the sum, average and standard deviation of these numbers. 
+   Call this function from the main() and print the results in the main() */
+
+#include <stdio.h>
+#include <math.h>
+
 //Function Prototype or declaration
-int basicNumberfunc(int,int,int,int,int); 
+double StandardDeviationfn(int, int, int, int, int);
 
 int main()
 {
@@ -11,12 +15,39 @@ int main()
 	
 	printf("\nEnter the 5 Number :");
 	scanf("%d %d %d %d %d",&inum1,&inum2,&inum3,&inum4,&inum5);
-	iAnswer =basicNumberfunc(inum1,inum2,inum3,inum4,inum5);
-	printf("\nSum of 5 Entered Number = %d",iAnswer);
-	printf("\nAverage of 5 Entered Number  %d/=5",iAnswer);
+	iAnswer = inum1+inum2+inum3+inum4+inum5;
+	printf("\nSum of 5 entered Number = %d",iAnswer);
+	printf("\nAverage of 5 entered Number  %d/5 = %d",iAnswer,iAnswer/5); //Since the count of the entered number is 5.
+	printf("\nStandard Deviation of the 5 entered number = %.2lf\r\n", StandardDeviationfn(inum1, inum2, inum3, inum4, inum5));
 }
 
-int basicNumberfunc(int a,int b, int c, int d, int e)
+
+/* Standard deviation is a statistical measure that quantifies the amount of dispersion or spread of a set of data points around their mean (average) 
+	
+Steps to Calculate Standard Deviation (Step-by-Step) :
+1. Calculate the Mean: Sum the numbers and divide by the count.
+2. Find Squared Deviations: For each number, subtract the mean and square the result (distance squared).
+3. Find Variance: Sum all squared deviations and divide by the count (population) or n-1(sample).
+4. Square Root: Take the square root of the variance
+
+Example:
+For data set {10, 11, 12, 13, 14}, the average is 12.
+
+1. Squares: (10-12)2=4, (11-12)2=1, (12-12)2=0, (13-12)2=1, (14-12)2=4. NOTE: where,(num-num)2 is square.
+2. Sum: 4+1+0+1+4 = 10.
+3. Divide (sample n-1): Here, we have 5 samples. Hence (5-1) = 4.
+						Then, 10 / 4 = 2.5 (Variance)
+4. Square-root: square-root of 2.5 = 1.58 (Standard deviation)
+
+*/
+double StandardDeviationfn(int num1, int num2, int num3, int num4, int num5)
 {
-	return a+b+c+d+e;
+	int average; 
+	double sum, variance, stddevresult = 0;
+
+	average = (num1 + num2 + num3 + num4 + num5) / 5;
+	sum = pow((num1 - average),2) + pow((num2 - average),2) + pow((num3 - average),2) + pow((num4 - average),2) + pow((num5 - average),2);
+	variance = sum / 4; //Since, We have 5 sample as input. Hence, (5-1) = 4. Therefore, we're dividing by 4.
+	stddevresult = sqrt(variance);
+	return stddevresult;
 }
